ee98f9867f934394f6e5cb72de283e9ecfec2704
Dave Carson <dave@onthefencedevelopment.com>
2016-08-18 19:29:56 +0100

    Initial upload of Simple Overlay Form Project
    Basic Modal Form for Xamarin.Forms

---
+++ SimpleOverlayForm/SimpleOverlayForm/SimpleOverlayForm/Home.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace SimpleOverlayForm
+{
+    public partial class Home : ContentPage
+    {
+        
+        public Home()
+        {
+            InitializeComponent();
+        }
+        
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            EnteredName.Text = string.Empty;
+
+            overlay.IsVisible = true;
+
+            EnteredName.Focus();
+        }
+
+        void OnOKButtonClicked(object sender, EventArgs args)
+        {
+            overlay.IsVisible = false;
+
+            DisplayAlert("Result", string.Format("You entered {0}", EnteredName.Text), "OK");
+        }
+
+        void OnCancelButtonClicked(object sender, EventArgs args)
+        {
+            overlay.IsVisible = false;
+        }
+    }
+}
